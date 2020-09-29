9ef8456bcd22df291e3758011ef1070743f6586f
Olivia Bellino <71726052+oliviabellino@users.noreply.github.com>
2020-09-29 14:38:56 -0400

    before loop added comment

--- averagecodeGITHUB.py
+++ averagecodeGITHUB.py
@@ -5,8 +5,12 @@ average = 0.0
 scorecount = 0 
 
 numberOfTests = int(input("please enter the number of test you want to average:"))
+
+#make this stuff repeat until scoreCount = numberofTests
 score = int(input("Please enter a score: "))
 scorecount = scorecount + 1
 total = total + score
+
+
 average  = total/scorecount
 print("the average is ",average)
